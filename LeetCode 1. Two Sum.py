185113931e6ae64f3f24860c018f9563ab159a0b
Jos-Low <lowdenj1@msu.edu>
2025-09-27 12:00:15 -0400

    Update LeetCode 1. Two Sum.py

--- LeetCode 1. Two Sum.py
+++ LeetCode 1. Two Sum.py
@@ -2,8 +2,26 @@ nums = [3,3]
 target = 6
 # Sample TestCase
 
+
+# Solution in O(n) time
+output = []
+map = {}
+for i, n in enumerate(nums):
+    if target - n in map:
+        output.append(map[target-n][0])
+        output.append(i)
+        return output
+    if n in map:
+        map[n].append(i)
+    else:
+        map[n] = [i] 
+
+
+# naive solution
+'''
 for i, n in enumerate(nums):
     s = target-n
     if s in nums[i+1:]:
         # Test Print
         print([i, nums[i+1:].index(s)+i+1])
+'''
